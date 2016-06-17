a2937d91297212025859fb81ea9fbf1d06ea1c14
boyboi86 <boyboi86@gmail.com>
2016-06-18 01:37:22 +0800

    added more features

--- script/controller.js
+++ script/controller.js
@@ -1,4 +1,6 @@
 //controllers
+//respective controller set for their routes
+//just create $scope and bind to html will do
 
 myApp.controller('homeCtrl',['$scope', function($scope){
     
@@ -7,13 +9,13 @@ myApp.controller('homeCtrl',['$scope', function($scope){
 }]);
 
 myApp.controller('proCtrl',['$scope', function($scope){
-    
+    $scope.description='mostly done in Javascript'
     
     
 }]);
 
 myApp.controller('contactCtrl',['$scope', function($scope){
-    
+    $scope.description=['Mean stack developer','Dog-lover', 'Avid reader']
     
     
 }]);
